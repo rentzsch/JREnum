222c4e7f124631d95ae6a16bb90e8ffad61ffc73
rentzsch <jwr.git@redshed.net>
2012-12-11 00:08:03 -0600

    Add another client for split enum to ensure lack of linker errors.

---
+++ JREnumTest/JREnumTest/SplitEnumClient.h
@@ -0,0 +1,13 @@
+//
+//  SplitEnumClient.h
+//  JREnumTest
+//
+//  Created by Wolf Rentzsch on 12/10/12.
+//  Copyright (c) 2012 Jonathan 'Wolf' Rentzsch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SplitEnumClient : NSObject
+
+@end
