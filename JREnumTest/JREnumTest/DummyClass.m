10abbdeaafe5b672ea5721b4e7bd67857927999b
Benedict Cohen <ben@benedictcohen.co.uk>
2015-03-02 10:20:20 +0000

    Added a dummy class that imports SplitEnums thus allowing for duplicate symbol errors to become apparent

---
+++ JREnumTest/JREnumTest/DummyClass.m
@@ -0,0 +1,15 @@
+//
+//  DummyClass.m
+//  JREnumTest
+//
+//  Created by Benedict Cohen on 02/03/2015.
+//  Copyright (c) 2015 Jonathan 'Wolf' Rentzsch. All rights reserved.
+//
+
+#import "DummyClass.h"
+
+
+
+@implementation DummyClass
+
+@end
